470dc94779f52289a7c84c10eae1253b04f24c9d
zt <ztowork9696@163.com>
2021-05-28 09:12:20 +0800

    5.28

--- scala/scala-demo/src/main/scala/com/atguigu/day06_函数/SimpleWordCount.scala
+++ scala/scala-demo/src/main/scala/com/atguigu/day06_函数/SimpleWordCount.scala
@@ -19,13 +19,13 @@ object SimpleWordCount {
     mapValues在2.13中被废弃需要添加toMap
      */
     val mapLen: MapView[String, Int] = group.mapValues((list: List[String]) => list.length)
-    println(mapLen.toMap)
+    println("mapVaule"+mapLen.toMap)
     /*
     替换为map直接统计,后面需要排序,所以需要先转换为list
      */
     //    val value: Any = group.map((tuple2:(String,List[String]))=> (tuple2._1,tuple2._2.length)).toList
     val list1: List[(String, Int)] = group.map(tuple2 => (tuple2._1, tuple2._2.length)).toList
-    println(list1)
+    println("map统计"+list1)
 
 
     //排序
